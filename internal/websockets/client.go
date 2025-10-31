4e4eebe299273610ba932a3f12c3cbb9f4e2d1da
ManogyaDahal <bat16692@gmail.com>
2025-10-31 18:39:30 +0545

    add: elaborated the logs to give detailed error

--- internal/websockets/client.go
+++ internal/websockets/client.go
@@ -3,6 +3,7 @@ package websockets
 import (
 	"log"
 	"time"
+	"net"
 
 	"github.com/gorilla/websocket"
 )
@@ -30,17 +31,27 @@ func (c *Clients) ReadPump() {
 	}()
 
 	c.connection.SetReadLimit(512) //protects oversized messages
-	c.connection.SetReadDeadline(time.Now().Add(pongWait)) // handle error
+	_ = c.connection.SetReadDeadline(time.Now().Add(pongWait)) // handle error
 	//reloads the timeout whenever client responds to pings
 	c.connection.SetPongHandler( func(string)error {
-		c.connection.SetReadDeadline(time.Now().Add(pongWait))	
+		_ = c.connection.SetReadDeadline(time.Now().Add(pongWait))	
 		return nil
 	})
 
 	for {
 		_, message, err := c.connection.ReadMessage()
+
+		// classifying the error to get specific error
 		if err != nil {
-			log.Println("[Client]: read error")
+			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
+				log.Printf("[Client %v]: unexpected close: %v", c.name, err)
+			} else if websocket.IsCloseError(err, websocket.CloseNormalClosure, websocket.CloseGoingAway) {
+				log.Printf("[Client %v]: normal closure", c.name)
+			} else if ne, ok := err.(net.Error); ok && ne.Timeout() {
+				log.Printf("[Client %v]: read timeout", c.name)
+			} else {
+				log.Printf("[Client %v]: read error: %v", c.name, err)
+			}
 			break	
 		}
 		c.hub.broadcast <- message
@@ -59,32 +70,52 @@ func(c *Clients) WritePump(){
 	for {
 		select {
 		case message, ok := <-c.send:
-			c.connection.SetWriteDeadline(time.Now().Add(writeWait))
+			_ = c.connection.SetWriteDeadline(time.Now().Add(writeWait))
 			if !ok {
-				c.connection.WriteMessage(websocket.CloseMessage, []byte{})
+				_ = c.connection.WriteMessage(websocket.CloseMessage, []byte{})
+				log.Printf("[Client %v]: send channel closed by hub", c.name)
 				return
 			}
 
 			w, err := c.connection.NextWriter(websocket.TextMessage)
 			if err != nil {
+				log.Printf("[Client %v]: write setup error: %v", c.name, err)
 				return 
 			}
-			w.Write(message)
+
+			//write main message
+			if _, err := w.Write(message); err != nil {
+				log.Printf("[Client %v]: write error: %v", c.name, err)
+				_ = w.Close()
+				return
+			}
 
 			//add queued message into same websocket frame
-			for  range len(c.send) {
-				w.Write([]byte{})
-				w.Write(<-c.send)
+			for i := 0; i < len(c.send); i++ {
+				nextMsg := <-c.send
+				//no seperator is used for the write
+				if _, err := w.Write([]byte{}); err == nil {
+					_, _ = w.Write(nextMsg)
+				}
 			}
 
 			if err := w.Close(); err != nil{
+				log.Printf("[Client %v]: writer close error: %v", c.name, err)
 				return
 			}
 
 		case <- ticker.C:
-			//send ping periodically to keep the connection alive 
+			//send ping periodically to keep the connection alive handeled by pong handler
 			c.connection.SetWriteDeadline(time.Now().Add(writeWait))
 			if err := c.connection.WriteMessage(websocket.PingMessage, []byte{}); err != nil {
+				// classify error
+				if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
+					log.Printf("[Client %v]: ping failed, connection lost: %v", c.name, err)
+				} else if ne, ok := err.(net.Error); ok && ne.Timeout() {
+					log.Printf("[Client %v]: ping timeout", c.name)
+				} else {
+					log.Printf("[Client %v]: write pump error: %v", c.name, err)
+				}
 				return
 			}
 		}
