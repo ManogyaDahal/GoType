d36a50ac5682884c325b276aad549cd2c3fe2c31
ManogyaDahal <bat16692@gmail.com>
2025-11-03 22:15:06 +0545

    Initialized frontend

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,12 @@
+// // src/App.jsx
+function App() {
+  return (
+    <div className="h-screen flex flex-col items-center justify-center bg-gray-900 text-white">
+      <h1 className="text-4xl font-bold">Go Type</h1>
+      <p className="mt-4 text-gray-300"> Welcome to the GoType application</p>
+    </div>
+  );
+}
+
+export default App;
+
